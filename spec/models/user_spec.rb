bcd471bd460f7475dbf8ba24f9751fd9b3d2e5d1
Evgeniy Demidov <eydemidov@gmail.com>
2024-07-11 05:42:26 +0200

    Add more explicit tests for user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,23 +4,43 @@ RSpec.describe User, type: :model do
   describe "validations" do
     subject(:user) { build(:user, name: "Blah") }
 
-    context "when name is nil" do
-      before { user.name = nil }
-      it { should_not be_valid }
-    end
-
-    context "when name is blank" do
-      before { user.name = " " }
-      it { should_not be_valid }
-    end
-
-    context "when name already exists, case-insensitive" do
-      before do
-        create(:user, name: "Gregorz")
-        user.name = "GReGoRZ"
+    context "name validations" do
+      context "when name is valid" do
+        it "doesn't add an error" do
+          expect(user).to be_valid
+          expect(user.errors[:name]).to_not include(I18n.t("errors.messages.blank"))
+        end
       end
 
-      it { should_not be_valid }
+      context "when name is nil" do
+        before { user.name = nil }
+
+        it "adds an error" do
+          expect(user).not_to be_valid
+          expect(user.errors[:name]).to include(I18n.t("errors.messages.blank"))
+        end
+      end
+
+      context "when name is blank" do
+        before { user.name = " " }
+
+        it "adds an error" do
+          expect(user).not_to be_valid
+          expect(user.errors[:name]).to include(I18n.t("errors.messages.blank"))
+        end
+      end
+
+      context "when name already exists, case-insensitive" do
+        before do
+          create(:user, name: "Gregorz")
+          user.name = "GReGoRZ"
+        end
+
+        it "adds an error" do
+          expect(user).not_to be_valid
+          expect(user.errors[:name]).to include(I18n.t("errors.messages.taken"))
+        end
+      end
     end
 
     context "password validations" do
